317d22beec228e99a06956fdcc4832fcaff4f8b6
discipuloJL <lucasenglish54@gmail.com>
2026-03-14 11:38:37 -0300

    Controller initial commit

--- src/main/java/com/jltodolist/todolist/controller/MinhaPrimeiraController.java
+++ src/main/java/com/jltodolist/todolist/controller/MinhaPrimeiraController.java
@@ -1,5 +1,25 @@
 package com.jltodolist.todolist.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/primeiraRota")
+// http://localhost:8080/primeiraRota --------
 public class MinhaPrimeiraController {
 
+    /**
+     * Métodos de acesso do HTTP
+     * GET - Buscar uma informação
+     * POST - Adicionar um dado/informação
+     * PUT - Alterar um dado/info
+     * DELETE - Remover um dado
+     * PATCH - Alterar somente uma parte da info/dado
+     */
+
+    @GetMapping("/primeiroMetodo")
+    public String primeiraMensagem() {
+        return "Funcionou";
+    }
 }
